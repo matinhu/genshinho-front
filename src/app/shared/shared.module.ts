03268015c07b325c06021261826777fb8a47c5da
Matheus <matheulls@hotmail.com>
2021-07-09 22:44:36 -0300

    cicd test

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -6,7 +6,6 @@ import { NzMenuModule } from 'ng-zorro-antd/menu';
 import { NzUploadModule } from 'ng-zorro-antd/upload';
 import { IconsProviderModule } from 'src/app/icons-provider.module';
 import { NzMessageModule } from 'ng-zorro-antd/message';
-import { PdfViewerModule } from 'ng2-pdf-viewer';
 import { NzProgressModule } from 'ng-zorro-antd/progress';
 import { NzStepsModule } from 'ng-zorro-antd/steps';
 import { NzPopoverModule } from 'ng-zorro-antd/popover';
@@ -26,7 +25,6 @@ import { LoginComponent } from '../pages/login/login.component';
     NzMenuModule,
     NzUploadModule,
     NzMessageModule,
-    PdfViewerModule,
     NzProgressModule,
     NzStepsModule,
     NzPopoverModule,
@@ -40,7 +38,6 @@ import { LoginComponent } from '../pages/login/login.component';
     NzMenuModule,
     NzUploadModule,
     NzMessageModule,
-    PdfViewerModule,
     NzProgressModule,
     NzStepsModule,
     NzPopoverModule,
